21b81c7eda4b22d7c1f4418a646245fc165b1e08
ESTBLC <estblcsk@gmail.com>
2019-01-26 10:58:58 +0000

    Build vect of shdr with copy of originals

--- src/elflib/elf_shdr.c
+++ src/elflib/elf_shdr.c
@@ -1,8 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
 #include <elf.h>
 
 #include <assert.h>
 
 #include "elflib.h"
+
+static Elf32_Shdr *shdr_cpy(Elf32_Shdr *shdr);
 
 Elf32_Shdr *elf_shdr(elf32_t *elf, uint16_t n)
 {
@@ -30,6 +34,7 @@ vect_t *shdr_list(elf32_t *elf)
             Elf32_Shdr *shdr = elf_shdr(elf, i);
 
             if (shdr->sh_type == SHT_PROGBITS || shdr->sh_type == SHT_NOBITS) {
+                shdr = shdr_cpy(shdr);
                 vect_append(shdr_vect, shdr);
             }
         }
@@ -37,4 +42,11 @@ vect_t *shdr_list(elf32_t *elf)
     }
 
     return shdr_vect;
+}
+
+static Elf32_Shdr *shdr_cpy(Elf32_Shdr *shdr)
+{
+    Elf32_Shdr *shdr_cpy = malloc(sizeof(*shdr_cpy));
+
+    return memcpy(shdr_cpy, shdr, sizeof(*shdr));
 }
