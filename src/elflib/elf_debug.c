a86ae19810b787a1e7b0659e8cd382d067732182
ESTBLC <estblcsk@gmail.com>
2019-01-29 00:12:35 +0000

    Full comment out of elf_debug file

--- src/elflib/elf_debug.c
+++ src/elflib/elf_debug.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <elf.h>
+/* #include <stdio.h> */
+/* #include <elf.h> */
 
-#include <assert.h>
+/* #include <assert.h> */
 
-#include "elflib.h"
+/* #include "elflib.h" */
 
-static char *shdr_name(elf32_t *elf, Elf32_Shdr *shdr);
-static char *shdr_type(Elf32_Shdr *shdr);
-static char *phdr_type(Elf32_Phdr *phdr);
+/* static char *shdr_name(elf32_t *elf, Elf32_Shdr *shdr); */
+/* static char *shdr_type(Elf32_Shdr *shdr); */
+/* static char *phdr_type(Elf32_Phdr *phdr); */
 
 /* void elf_print(elf32_t *elf) */
 /* { */
@@ -71,55 +71,55 @@ static char *phdr_type(Elf32_Phdr *phdr);
 /*     } */
 /* } */
 
-static char *shdr_name(elf32_t *elf, Elf32_Shdr *shdr)
-{
-    Elf32_Ehdr *ehdr = elf_ehdr(elf);
-    Elf32_Shdr *name_section = elf_shdr(elf, ehdr->e_shstrndx);
+/* static char *shdr_name(elf32_t *elf, Elf32_Shdr *shdr) */
+/* { */
+/*     Elf32_Ehdr *ehdr = elf_ehdr(elf); */
+/*     Elf32_Shdr *name_section = elf_shdr(elf, ehdr->e_shstrndx); */
 
-    char *name_table = (char *)(elf->buf + name_section->sh_offset);
+/*     char *name_table = (char *)(elf->buf + name_section->sh_offset); */
 
-    return &name_table[shdr->sh_name];
-}
+/*     return &name_table[shdr->sh_name]; */
+/* } */
 
-static char *shdr_type(Elf32_Shdr *shdr) {
-    switch (shdr->sh_type) {
-        case SHT_NULL :
-            return "NULL";
-        case SHT_PROGBITS :
-            return "PROGBITS";
-        case SHT_SYMTAB :
-            return "SYMTAB";
-        case SHT_STRTAB :
-            return "STRTAB";
-        case SHT_RELA :
-            return "RELA";
-        case SHT_HASH :
-            return "HASH";
-        case SHT_DYNAMIC :
-            return "DYNAMIC";
-        case SHT_NOTE :
-            return "NOTE";
-        case SHT_NOBITS :
-            return "NOBITS";
-        case SHT_REL :
-            return "REL";
-        case SHT_SHLIB :
-            return "SHLIB";
-        case SHT_LOUSER :
-            return "LOUSER";
-        case SHT_HIUSER :
-            return "HIUSER";
-        default :
-            return "Not implemented !";
-    }
+/* static char *shdr_type(Elf32_Shdr *shdr) { */
+/*     switch (shdr->sh_type) { */
+/*         case SHT_NULL : */
+/*             return "NULL"; */
+/*         case SHT_PROGBITS : */
+/*             return "PROGBITS"; */
+/*         case SHT_SYMTAB : */
+/*             return "SYMTAB"; */
+/*         case SHT_STRTAB : */
+/*             return "STRTAB"; */
+/*         case SHT_RELA : */
+/*             return "RELA"; */
+/*         case SHT_HASH : */
+/*             return "HASH"; */
+/*         case SHT_DYNAMIC : */
+/*             return "DYNAMIC"; */
+/*         case SHT_NOTE : */
+/*             return "NOTE"; */
+/*         case SHT_NOBITS : */
+/*             return "NOBITS"; */
+/*         case SHT_REL : */
+/*             return "REL"; */
+/*         case SHT_SHLIB : */
+/*             return "SHLIB"; */
+/*         case SHT_LOUSER : */
+/*             return "LOUSER"; */
+/*         case SHT_HIUSER : */
+/*             return "HIUSER"; */
+/*         default : */
+/*             return "Not implemented !"; */
+/*     } */
 
-}
+/* } */
 
-static char *phdr_type(Elf32_Phdr *phdr) {
-    switch (phdr->p_type) {
-        case PT_LOAD:
-            return "LOAD";
-        default :
-            return "Not implemented !";
-    }
-}
+/* static char *phdr_type(Elf32_Phdr *phdr) { */
+/*     switch (phdr->p_type) { */
+/*         case PT_LOAD: */
+/*             return "LOAD"; */
+/*         default : */
+/*             return "Not implemented !"; */
+/*     } */
+/* } */
